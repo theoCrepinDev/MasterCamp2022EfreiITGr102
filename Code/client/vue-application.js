cb8ae2504ab966eea3704989858f785affc6cf78
gabrielbrancato <73238169+gabrielbrancato@users.noreply.github.com>
2022-06-13 12:54:33 +0200

    Début chelou de l'inscription

--- Code/client/vue-application.js
+++ Code/client/vue-application.js
@@ -1,7 +1,9 @@
 const Accueil = window.httpVueLoader("./components/Accueil.vue")
+const Login = window.httpVueLoader('./components/login.vue')
 
 const routes =[
-    {path : '/', component: Accueil}
+    {path : '/', component: Accueil},
+    { path: '/login', component: Login, name: 'login' }
 ]
 
 const router = new VueRouter({
@@ -16,6 +18,18 @@ var app = new Vue({
     async mounted(){
     },
     methods:{
-        
+        async login(data) {
+            await axios.post('/api/login', data).then(async response => {
+                this.user = response.data.data;
+                console.log(this.user.type)
+                //this.panier.articles = response.data.panier;
+                //router.replace('/catalogue');
+            }).catch(error => {
+                alert('Veuillez vérifiez vos information')
+                this.invaliddata = true
+            });
+          
+        }
+    }   
     }
-})
+)
--- Code/server/routes/api.js
+++ Code/server/routes/api.js
@@ -4,3 +4,37 @@ const mysql = require('mysql');
 const bcrypt = require('bcrypt');
 
 module.exports = router;
+
+
+
+  //Inscription d'un nouvel utilisateur
+  router.post('/register', async (req, res) => {
+    const nom = req.body.nom
+    const prenom = req.body.prenom
+    const email = req.body.email
+    const password = req.body.motDePasse
+    const hash = await bcrypt.hash(password, 10);
+    var type = 'user'
+  
+    //on regarde si cet utilisateur existe déjà
+    var sql ='SELECT * FROM user WHERE mail= ?'
+    var value = [email]
+    await db.query(sql, [value], async (err,result, fields) => {
+        if (err) throw err;
+        if(result.length > 0){
+            res.status(401).json({
+                message: 'Utilisateur déjà enregistré, essayez de vous connecter !'
+            })
+            return
+        }
+        else{
+            var sql = 'INSERT INTO user ( nom,prenom,email, password, type) VALUES ?'
+            var value =[[email, hash, type]];
+            await db.query(sql, [value], function(err,result) {
+                if (err) throw err;
+                res.send('il est enregistré')
+            })
+            return true
+        }
+    })
+  })
