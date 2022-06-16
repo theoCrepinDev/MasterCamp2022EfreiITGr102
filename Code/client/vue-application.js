cb0558b665bbe284c58fcf0fddcade34f06f5186
Dylan <dylan.mouheb@efrei.net>
2022-06-16 23:04:20 +0200

    Manque fond gris

--- Code/client/vue-application.js
+++ Code/client/vue-application.js
@@ -1,7 +1,9 @@
 const Accueil = window.httpVueLoader("./components/Accueil.vue")
+const Login = window.httpVueLoader('./components/login.vue')
 
-const routes =[
-    {path : '/', component: Accueil}
+const routes = [
+    { path: '/', component: Accueil },
+    { path: '/login', component: Login, name: 'login' }
 ]
 
 const router = new VueRouter({
@@ -11,11 +13,20 @@ const router = new VueRouter({
 var app = new Vue({
     router,
     el: '#app',
-    data:{
-    },
-    async mounted(){
-    },
-    methods:{
-        
+    data: {},
+    async mounted() {},
+    methods: {
+        async login(data) {
+            await axios.post('/api/login', data).then(async response => {
+                this.user = response.data.data;
+                console.log(this.user.type)
+                    //this.panier.articles = response.data.panier;
+                    //router.replace('/catalogue');
+            }).catch(error => {
+                alert('Veuillez vérifiez vos information')
+                this.invaliddata = true
+            });
+
+        }
     }
 })
