0d2b47ebf03a49a20a2f20af09fcd8254f28051c
Dylan <dylan.mouheb@efrei.net>
2022-06-21 23:16:25 +0200

    Nouvelle page de connexion

--- Code/client/components/Login.vue
+++ Code/client/components/Login.vue
@@ -1,49 +1,51 @@
 <template>
-<div class="container row">
-  <div class="row">
-    <div id="image-fond" class="col col-md-6" src="../assets/logo.jpg"></div>
-  </div>
+<div class="container row div-page-connexion">
+  <div id="image-fond" class="col-md-6 col-lg-6"></div>
+<div class="col-md-6 col-lg-6">
   
-    <div class="col-md-15">
-    <form id="form-connexion">
+   <form id="form-connexion">
       <h1 style="color:#E30000;">BIENVENUE</h1>
       <h2 style="color:#63634;">Entrez vos informations !</h2>
 
       <div class="form-group1">
-        <label for="InputEmail1">Email</label>
-        <input type="email" class="form-control" id="InputEmail" aria-describedby="Email" placeholder="Entrez votre email">
+        <label for="InputEmail1">Numéro CNI</label>
+        <input type="CNI" class="form-control" id="InputCNI" aria-describedby="CNI" placeholder="Entrez votre numéro CNI">
       </div>
 
       <div class="form-group2">
         <label for="InputPassword">Mot de passe</label>
         <input type="password" class="form-control" id="InputPassword" placeholder="*********" autofocus="" required="">
       </div>
-
-  <button type="Connexion" class="btn btn-primary btn-block">Connexion</button>
-  <p class="lien"> Vous n'avez pas de compte ? <a href="/Inscription" style="color:#E30000;" >Inscrivez-vous gratuitement </a> </p>
+      <button type="Connexion" class="btn btn-danger btn-block">Connexion</button>
+        <p class="lien"> Vous n'avez pas de compte ? <a href="/Inscription" style="color:#E30000;" >Inscrivez-vous gratuitement </a> </p>
+      
 </form> 
-  </div>
+
+</div>
 </div>
 </template>
 
 <style scoped>
 
 
-#image-fond{
-  position: absolute;
-  width: 380px;
-  height: 414.77px;
-  margin-right: 6%; 
+
+
+#image-fond {
+background-image:url("../assets/logo.png");
+background-color: rgba(234, 234, 234, 1);
+background-repeat: no-repeat;
+background-size: contain;
+margin-left: auto;
+margin-right: -500px;;
+max-width: 900%;
+
 }
-/*
-.col-md-6 {
-  background-color:#EAEAEA;
-}
-*/
+
 #form-connexion {
-  width:25%;
-  margin-top:10%;
-  margin-left:12%; 
+  width:80%;
+  margin-top:13%;
+  margin-left:-120%; 
+  height: 610px;
 }
 
 h1 {
@@ -61,12 +63,10 @@ h2 {
   margin-top: -3.5%;
 }
 
-.btn-primary{
-  margin-left: 110px;
-  margin-top: 15%;
-  width: 45%;
-  background-color: #E30000;
-  height: 45px;
+.btn-danger{
+  margin-left: 22.5%;
+  margin-top: 5%;
+  width: 60%;
 
 }
 
@@ -79,9 +79,14 @@ h2 {
 }
 
 .lien{
-  margin-left: 40px;
-  margin-top: 10%;
+  margin-left: 50px;
+  margin-top: 5%;
+  font-family: "Gill Sans", sans-serif;
   font-size: smaller;
 }
-
+.div-page-connexion{
+  width: 70%;
+  margin-left: auto;
+  margin-right: auto;
+}
 </style>
